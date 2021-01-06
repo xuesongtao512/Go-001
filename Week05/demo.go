e9fe27876fb646c0e68d8820ee9149419e59c9c8
xuesongtao512@qq.com <xuesongtao512@qq.com>
2021-01-06 22:36:43 +0800

    调整限流器

--- Week05/demo.go
+++ Week05/demo.go
@@ -1,36 +1,43 @@
-// @Desc: 基于
-// @CreateTime: 2021/1/6
+// @Desc: 基于时间滑动窗口实现
+// @CreateTime: 2021/1/4
 package Week05
 
 import "time"
 
-// 存放队列
-var LimitQueue map[string][]int64
+type RateLimit struct {
+	appName     string  // 应用名
+	limitQueue  []int64 // 存放队列
+	limitNum    int     // 限制数量
+	limitDuring int64   // 限制的时间
+}
 
-func IsLimit(queueName string, limitNum int, limitDuring int64) bool {
-    currTime := time.Now().Unix()
-    // 如果为 nil 的话就进行初始化
-    if LimitQueue == nil {
-        LimitQueue = make(map[string][]int64, 0)
-    }
-    // 如果队列的值不存在的话就初始化
-    if _, ok := LimitQueue[queueName]; !ok {
-        LimitQueue[queueName] = make([]int64, 0)
-    }
-    // 如果队列未满的话就进行添加
-    if len(LimitQueue[queueName]) < limitNum {
-        LimitQueue[queueName] = append(LimitQueue[queueName], currTime)
-        return true
-    }
-    // 满了的话, 需要将第一个与当前进行比较, 如果大于设置的限制时间, 就需要剔除第一个值, 从新加入
-    // 否则就不能
-    earlyTime := LimitQueue[queueName][0]
-    // 不在限定时间范围内
-    if currTime - earlyTime > limitDuring {
-        // 删除第一个
-        LimitQueue[queueName] = LimitQueue[queueName][1:]
-        LimitQueue[queueName] = append(LimitQueue[queueName], currTime)
-        return true
-    }
-    return false
+// 初始化对象
+func NewRateLimit(appName string, limitNum int, limitDuring int64) *RateLimit {
+	return &RateLimit{
+		appName:     appName,
+		limitQueue:  make([]int64, 0),
+		limitNum:    limitNum,
+		limitDuring: limitDuring,
+	}
+}
+
+// 获取令牌
+func (r *RateLimit) GetToken() bool {
+	currTime := time.Now().Unix()
+	// 如果队列未满的话就进行添加
+	if len(r.limitQueue) < r.limitNum {
+		r.limitQueue = append(r.limitQueue, currTime)
+		return true
+	}
+	// 满了的话, 需要将第一个与当前进行比较, 如果大于设置的限制时间, 就需要剔除第一个值, 从新加入
+	// 否则就不能
+	earlyTime := r.limitQueue[0]
+	// 不在限定时间范围内
+	if currTime-earlyTime > r.limitDuring {
+		// 删除第一个
+		r.limitQueue = r.limitQueue[1:]
+		r.limitQueue = append(r.limitQueue, currTime)
+		return true
+	}
+	return false
 }
--- Week05/demo_test.go
+++ Week05/demo_test.go
@@ -3,17 +3,19 @@
 package Week05
 
 import (
-    "log"
-    "testing"
+	"log"
+	"testing"
 )
 
-func TestIsLimit(t *testing.T) {
-    for i := 0; i < 15; i++ {
-        isOk := IsLimit("test", 10, 5)
-        if isOk {
-            log.Println("运行正常")
-        } else {
-            log.Println("运行限制")
-        }
-    }
+func TestRateLimit_GetToken(t *testing.T) {
+	// 初始化一个 10s 内只有 10 个的限流器
+	rateLimit := NewRateLimit("test", 10, 10)
+	for i := 0; i < 15; i++ {
+		isOk := rateLimit.GetToken()
+		if isOk {
+			log.Println("运行正常")
+		} else {
+			log.Println("运行限制")
+		}
+	}
 }
